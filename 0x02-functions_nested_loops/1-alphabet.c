9a8cf17846f822692cdc9378bf0d93275df35e2f
dan-musembi <danielmusembi427@gmail.com>
2023-03-21 01:37:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - creates characters
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'r'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
